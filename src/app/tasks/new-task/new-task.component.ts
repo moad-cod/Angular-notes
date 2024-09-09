f260561ae575aca1192279c3ceddf5ef25a2f400
moad-cod <elbaz.ouad1249@gmail.com>
2024-09-09 10:09:33 +0100

    Ch 51-54 Using Directives/Signals & Two-Way-Binding , Handling Form Submission 09/9/2024

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  // directives & Two-Way-Binding 
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+ 
+ 
+
   onCancel() {
     this.cancel.emit();
   }
+
+  onSubmit() {
+    
+  }
+  // signals & Two-Way-Binding 
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDate = signal('');
 }
